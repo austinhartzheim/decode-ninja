c36a4d5cdf0f2bbb16cd96f6bcf975be1e1e6598
Austin Hartzheim <austin@austinhartzheim.me>
2016-02-20 12:48:46 +0000

    Added rule to colorize null bytes.

--- web/js/rules.js
+++ web/js/rules.js
@@ -26,3 +26,30 @@ rules.push({
     desc: 'Find newline characters in a file and highlight them.',
     obj: RuleHighlightNewLines
 });
+
+
+function RuleHighlightNullBytes() {
+    this.fields = {
+        color_fg: {
+            name: 'Foreground Color',
+            value: 'white'
+        },
+        color_bg: {
+            name: 'Background Color',
+            value: 'red'
+        }
+    };
+
+    this,name = 'Highlight Null Bytes'
+    this.apply = function(bytes, i) {
+        if (bytes[i].d == 0) {
+            bytes[i].style.background = this.fields.color_bg.value;
+            bytes[i].style.color = this.fields.color_fg.value;
+        }
+    };
+}
+rules.push({
+    name: 'Highlight Null Bytes',
+    desc: 'Highlight all null (zero) bytes in a file.',
+    obj: RuleHighlightNullBytes
+});
